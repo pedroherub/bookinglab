3c3924da2105c4f0a1f29d68b70eaf4b473352b2
pedroherub <pedroherub@gmail.com>
2010-12-26 04:45:18 +0700

    Last commit

--- app/models/block.rb
+++
@@ -1,5 +0,0 @@
-class Block < ActiveRecord::Base
-	attr_accessible :name
-	has_many :rooms, :dependent => :destroy
-        has_many :computers, :through => :room
-end
--- app/models/room.rb
+++
@@ -1,5 +0,0 @@
-class Room < ActiveRecord::Base
-	attr_accessible :name, :block_id
-	belongs_to :block
-	has_many :computers, :dependent => :destroy
-end
