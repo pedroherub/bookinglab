a5a2c60def22a41dcd74bd2ed7020c750e5179c1
pedroherub <pedroherub@gmail.com>
2014-03-25 21:54:38 +0000

    Update secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,28 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Bookinglab::Application.config.secret_token = '01e937e83eb0372ffa2b16fc2968721552e23e769050acce059c2a4399d921d8679d5e5ec52b21725b038c1f4a6bafe14aa46906fb9416fc6aab1d7836959431'
+
+
+# Be sure to restart your server when you modify this file.
+
+require 'securerandom'
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+
+def find_secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist? token_file
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token of 64 random hexadecimal characters and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Bookinglab::Application.config.secret_token = find_secure_token
